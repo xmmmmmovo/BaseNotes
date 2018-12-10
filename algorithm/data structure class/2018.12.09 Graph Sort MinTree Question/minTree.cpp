73bc512b42e0b6373b1242d0fd308e0b3d4ae6e5
xmmmmmovo <13256172360@163.com>
2018-12-10 16:12:51 +0800

    change

--- algorithm/data structure class/2018.12.09 Graph Sort MinTree Question/minTree.cpp
+++ algorithm/data structure class/2018.12.09 Graph Sort MinTree Question/minTree.cpp
@@ -1,13 +1,13 @@
 /**
  * 最小生成树
- * language : c++ author : xmmmmmovo
+ * language : c author : xmmmmmovo
  * 2018-12-09
 */
 #include <cstdio>
 #include <cmath>
 #include <cstdlib>
 
-#define INFIN -1 // 定义无穷为-1
+#define INFIN 5000 // 定义无穷为5000 
 #define MAX 20 // 定义最大节点是20个
 using namespace std;
 
@@ -16,29 +16,31 @@ typedef char vertextype; // 顶点类型
 typedef int edgetype; // 权值类型
 typedef struct
 {
-    vertextype vexs[MAX];
-    edgetype edges[MAX][MAX];
+    vertextype vexs[MAX]; // 点
+    edgetype edges[MAX][MAX]; // 边
     int nos, edgeNum; // 顶点, 边数
 }Mgraph;
 
-Mgraph *graph;
+Mgraph *graph = new Mgraph();
 
 void create(int c){
-    FILE *fp = fopen("./data.txt", "rw");
+    FILE *fp = fopen("./data.txt", "r");
     int weight = 0;
 
     if(fp){
-        fscanf(fp, "%d%d", &graph->nos, &graph->edgeNum); // 读取点跟边的数量
+        fscanf(fp, "%d %d", &graph->nos, &graph->edgeNum); // 读取点跟边的数量
         for(i = 0;i < graph->nos;i++){
             fscanf(fp, "%1s", &graph->vexs[i]);
         }
-        for(i = 0;i < graph->nos;i++){ 
+
+        for(i = 0;i < graph->nos;i++){ // 初始化矩阵
             for(j = 0;j < graph->nos; j++){
-                graph->edges[i][j] = i == j ? 0 : INFIN;
+                graph->edges[i][j] = (i == j ? 0 : INFIN);
             }
         }
-        for(k = 0;k < graph->edgeNum; k++){
-            fscanf(fp, "%d%d%d", &i, &j, &weight);
+
+        for(k = 0;k < graph->edgeNum; k++){ // 进行文件读取输入
+            fscanf(fp, "%d %d %d", &i, &j, &weight);
             graph->edges[i][j] = weight;
             if(c == 0)
                 graph->edges[j][i] = weight;
@@ -51,50 +53,57 @@ void create(int c){
 
 typedef struct edgedata
 {
-    int begin, end; // 定点序号
+    int begin, end; // 点序号
     int length; // 边长
 }edge;
 
 void prim(){
-    edge tree[MAX - 1];
+    edge tree[MAX - 1]; // 用来存树
     edge x;
     int d, min, j, k, s, v;
 
-    for(v = 0; v < graph->nos; v++){
-        tree[v - 1].begin = 0;
-        tree[v - 1].end = v;
-        tree[v - 1].length = graph->edges[0][v];
+    for(v = 0; v < graph->nos - 1; v++){
+        tree[v].begin = 0;
+        tree[v].end = v + 1;
+        tree[v].length = graph->edges[0][v + 1];
     }
 
     for(k = 0; k < graph->nos - 2; k++){
         min = tree[k].length;
         s = k;
-        for(j = k + 1; j < graph->nos - 1; j++){
+        for(j = k + 1; j < graph->nos - 1; j++){ // 选取最短路径
             if(tree[j].length < min){
                 min = tree[j].length;
                 s = j;
             }
         }
-        v = tree[s].end; // 把min当成入点
+        v = tree[s].end; // 入点就是最短路径点的末端点
 
+        // 交换把最短路径放到第一点后面
         x = tree[s];
         tree[s] = tree[k];
+        tree[k] = x;
 
-        for(j = k + 1; j < graph->nos - 1; j++){
+        // 交换完k就是最短边
+        for(j = k + 1; j < graph->nos - 1; j++){ // 更新两栖边的信息
             d = graph->edges[v][tree[j].end];
             if(d < tree[j].length){
                 tree[j].length = d;
                 tree[j].begin = v;
             }
         }
-    }
-    printf("min生成树是:\n");
+    } 
+    printf("min is:\n");
     for(j = 0; j < graph->nos - 1; j++){
         printf("%c --- %c %d\n", graph->vexs[tree[j].begin], 
         graph->vexs[tree[j].end], tree[j].length);
 
-        printf("根节点是: %c\n", graph->vexs[0]);
+        printf("root: %c\n", graph->vexs[0]);
     }
+}
+
+void kruskal(){
+    
 }
 
 int main(int argc, char const *argv[])
