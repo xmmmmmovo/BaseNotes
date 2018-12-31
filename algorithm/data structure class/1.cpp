b42e4f067e7b6e6b5aecff67f1544d7252bcb76b
xmmmmmovo <13256172360@163.com>
2018-12-31 12:25:07 +0800

    add tree

---
+++ algorithm/data structure class/1.cpp
@@ -0,0 +1,19 @@
+/**
+  工具类
+  里面有各种算法的函数
+*/
+#include <cstdio>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int *a;
+
+    printf("%d\n", a);
+    a = new int;
+
+    printf("%d\n", a);
+
+    system("pause");
+    return 0;
+}
--- algorithm/data structure class/test.cpp
+++ algorithm/data structure class/test.cpp
@@ -1,5 +1,51 @@
-#include "treeitem.h"
-#include "treemodel.h"
+TreeItem::TreeItem(const QList &data, TreeItem *parent)
+{
+    parentItem = parent;
+    itemData = data;
+}
+
+TreeItem::~TreeItem()
+{
+    qDeleteAll(childItems);
+}
+
+void TreeItem::a(TreeItem *item)
+{
+    childItems.append(item);
+}
+
+TreeItem *TreeItem::child(int row)
+{
+    return childItems.value(row);
+}
+
+int TreeItem::childCount() const
+{
+    return childItems.count();
+}
+
+int TreeItem::columnCount() const
+{
+    return itemData.count();
+}
+
+QVariant TreeItem::data(int column) const
+{
+    return itemData.value(column);
+}
+
+TreeItem *TreeItem::parent()
+{
+    return parentItem;
+}
+
+int TreeItem::row() const
+{
+    if (parentItem)
+        return parentItem->childItems.indexOf(const_cast(this));
+
+    return 0;
+}
 
 TreeModel::TreeModel(QObject *parent)
     : QAbstractItemModel(parent)
