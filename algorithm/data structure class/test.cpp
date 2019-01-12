ac8f36615ef610fbba8a83b047bf30743945b1bb
xmmmmmovo <13256172360@163.com>
2019-01-12 19:15:07 +0800

    1

---
+++ algorithm/data structure class/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "main" << endl;
+
+    system("pause");
+    return 0;
+}
