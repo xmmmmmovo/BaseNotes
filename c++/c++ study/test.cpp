e7b4fee7a8fa0576398bc2e1862cd54e636d4f9a
xmmmmmovo <13256172360@163.com>
2019-02-24 22:18:06 +0800

    can't over

---
+++ c++/c++ study/test.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+
+int main(int argc, char **argv)
+
+{
+    int printf = -1;
+    std::cout << printf << '\n';
+
+    system("pause");
+    return 0;
+}
