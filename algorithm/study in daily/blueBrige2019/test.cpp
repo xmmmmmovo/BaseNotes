88cf1ffc88773f9c454778cbcdce426cacdfe732
xmmmmmovo <13256172360@163.com>
2019-02-27 10:39:20 +0800

    easy to start flutter

--- algorithm/study in daily/blueBrige2019/test.cpp
+++ algorithm/study in daily/blueBrige2019/test.cpp
@@ -1,10 +1,26 @@
-#include <iostream>
-#include <cstdlib>
+/**
+ * author: xmmmmmovo
+ * generation time: 2019/02/27
+ * filename: test.cpp
+*/
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<algorithm>
+#include<map>
+#include<vector>
+#include<queue>
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
+int main(){
+#ifdef ONLINE_JUDGE
+#else
+    freopen("1.txt", "r", stdin);
+#endif
+    
 
     system("pause");
     return 0;
