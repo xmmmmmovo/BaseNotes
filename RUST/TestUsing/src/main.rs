a4de38f5208d04de841e30c83e43cd83d991277f
xmmmmmovo <13256172360@163.com>
2019-07-06 13:28:50 +0800

    rust

--- RUST/TestUsing/src/main.rs
+++ RUST/TestUsing/src/main.rs
@@ -1,4 +1,7 @@
-
 // 测试用的程序
 fn main() {
+    let mut v = vec![1, 2, 3, 4, 5];
+    for i in v.iter() {
+    }
+    let x = v.pop().expect("cant pop");
 }
--- algorithm/leetcode/1.两数之和.cpp
+++ algorithm/leetcode/1.两数之和.cpp
@@ -3,22 +3,14 @@
  *
  * [1] 两数之和
  */
+
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
-            }
-        }
-        return ans;
+        
     }
 };
+
