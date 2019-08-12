6ab469f3cbc1e2805fd86fdc1342c74d14feaf98
xmmmmmovo <13256172360@163.com>
2019-08-12 11:23:42 +0800

    go

--- algorithm/study in daily/test.cpp
+++ algorithm/study in daily/test.cpp
@@ -14,5 +14,16 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
+    int a = 0, b = 0, c = 0;
+    double unknow = 0.0;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    while (a * unknow + b != c) {
+        unknow = unknow + 0.01;
+    }
+
+    printf("%lf", &unknow);
+
     return 0;
 }
---
+++ go/study/defer_recover.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func test1() {
+	defer func() {
+		// 两种写法都可以
+		//err := recover()
+		if err := recover(); err != nil {
+			fmt.Println("err = ", err)
+		}
+	}()
+
+	num1 := 1
+	num2 := 0
+	res := num1 / num2
+	fmt.Println(res)
+}
+
+// 自定义异常 interface 类型
+func read(name string) (err error) {
+	if name == "config.ini" {
+		return nil
+	}else {
+		return errors.New("错误!")
+	}
+}
+
+func test2() {
+	err := read("config.ini")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("success!")
+}
+
+func main() {
+	//test1()
+	test2()
+}
